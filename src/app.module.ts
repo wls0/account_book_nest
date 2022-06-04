6e88f075e21c06879e03263ad3796d8f2eb07f1d
wls0 <kjy0836@naver.com>
2022-06-04 22:16:26 +0900

    typeOrm 설정, filter, interceptors pipes 사용 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { LoginModule } from './login/login.module';
-import { AccountsModule } from './accounts/accounts.module';
-import { UsersModule } from './users/users.module';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { LoginModule } from './login/login.module'
+import { AccountsModule } from './accounts/accounts.module'
+import { UsersModule } from './users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Accounts } from './model/accounts.model'
+import { Users } from './model/users.model'
 
 @Module({
-  imports: [LoginModule, AccountsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB,
+      entities: [Accounts, Users],
+      synchronize: process.env.NODE_ENV === 'local' ? true : false,
+    }),
+    LoginModule,
+    AccountsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { ErrorExceptionFilter } from './common/filters/error.exception'
+import { SuccessInterceptor } from './common/interceptors/success.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalFilters(new ErrorExceptionFilter())
+  app.useGlobalInterceptors(new SuccessInterceptor())
+  app.useGlobalPipes(new ValidationPipe())
+  await app.listen(3000)
 }
-bootstrap();
+
+bootstrap()
